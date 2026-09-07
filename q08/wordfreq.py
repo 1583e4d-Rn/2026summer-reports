827339492f1858787443e3e7cdb656d7331c33ed
1583e4d-Rn <wsr5566@outlook.com>
2026-09-07 13:27:21 +0800

    question 8 modified

--- q08/wordfreq.py
+++ q08/wordfreq.py
@@ -1,8 +1,3 @@
 words = open("words.txt", encoding="utf-8").read().split()
-seen = set()
-unique = []
-for word in words:
-    if word not in seen:
-        seen.add(word)
-        unique.append(word)
+unique = set(words)
 print("count=", len(unique))
